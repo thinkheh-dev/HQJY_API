b1df9c089f2fa85873b9aaf5ce17270f95acbef3
warlock921 <caoyu921@163.com>
2018-12-14 16:48:41 +0800

    添加个xadmin的migrations文件夹

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 from django.db.models import Q
@@ -24,12 +25,14 @@ class CustomBackend(ModelBackend):
 	"""
     自定义用户验证
     """
-	
+
 	def authenticate(self, request, username=None, password=None, **kwargs):
 		try:
 			user = User.objects.get(Q(username=username) | Q(user_phone=username))
 			if user.check_password(password):
 				return user
+			else:
+				return None
 		except Exception as e:
 			return None
 
