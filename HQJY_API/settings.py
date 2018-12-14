08d40882cd1bc11a7f56e902835084181ac9968a
warlock921 <caoyu921@163.com>
2018-12-14 11:23:54 +0800

    12.14 11:23修改

--- HQJY_API/settings.py
+++ HQJY_API/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
 		'USER': 'root',
 		'PASSWORD': 'P@ssword',
 		'HOST': '192.168.10.201',
-		'OPTIONS': { 'init_command': 'SET storage_engine=INNODB;' } ,
+		#'OPTIONS': { 'init_command': 'SET storage_engine=INNODB;' } ,
 	}
 }
 
