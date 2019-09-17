d2fc797448ef3e494732998def8a5d0b5752929a
warlock921 <caoyu921@163.com>
2019-09-17 10:53:51 +0800

    修复金融用户收藏BUG

--- apps/user_operation/signals.py
+++ apps/user_operation/signals.py
@@ -12,28 +12,56 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import UserFav
 from service_object.models import DefaultServices
+from service_object.models import FinancingServices
 
 
 @receiver(post_save, sender=UserFav)
 def create_user_fav(sender, instance=UserFav, created=False, **kwargs):
 
 	if created:
-
-		print("服务产品 {} 被收藏 ".format(instance.default_services.service_sn))
-		fav_num = DefaultServices.objects.get(service_sn=instance.default_services.service_sn).service_fav_nums
-		print("服务产品 {} 当前收藏数为: {} ".format(instance.default_services.service_sn, fav_num))
-		fav_num += 1
+		if instance.default_services is not None:
+			
+			print("服务产品 {} 被收藏 ".format(instance.default_services.service_sn))
+			fav_num = DefaultServices.objects.get(service_sn=instance.default_services.service_sn).service_fav_nums
+			print("服务产品 {} 当前收藏数为: {} ".format(instance.default_services.service_sn, fav_num))
+			fav_num += 1
 	
-		DefaultServices.objects.filter(service_sn=instance.default_services.service_sn).update(service_fav_nums=fav_num)
-		print("服务产品 {} 收藏数更新后为: {} ".format(instance.default_services.service_sn, fav_num))
+			DefaultServices.objects.filter(service_sn=instance.default_services.service_sn).update(service_fav_nums=fav_num)
+			print("服务产品 {} 收藏数更新后为: {} ".format(instance.default_services.service_sn, fav_num))
+		else:
+			print("服务产品 {} 被收藏 ".format(instance.financing_services.service_sn))
+			fav_num = FinancingServices.objects.get(service_sn=instance.financing_services.service_sn).service_fav_nums
+			print("服务产品 {} 当前收藏数为: {} ".format(instance.financing_services.service_sn, fav_num))
+			fav_num += 1
+			
+			FinancingServices.objects.filter(service_sn=instance.financing_services.service_sn).update(
+				service_fav_nums=fav_num)
+			print("服务产品 {} 收藏数更新后为: {} ".format(instance.financing_services.service_sn, fav_num))
 
 
 @receiver(post_delete, sender=UserFav)
 def delete_user_fav(sender, instance=UserFav, **kwargs):
-	
-	print("服务产品 {} 的收藏将被删除 ".format(instance.default_services.service_sn))
-	fav_num = DefaultServices.objects.get(service_sn=instance.default_services.service_sn).service_fav_nums
-	print("服务产品 {} 当前收藏数为: {} ".format(instance.default_services.service_sn, fav_num))
-	fav_num -= 1
-	DefaultServices.objects.filter(service_sn=instance.default_services.service_sn).update(service_fav_nums=fav_num)
-	print("服务产品 {} 收藏数更新后为: {} ".format(instance.default_services.service_sn, fav_num))
+	if instance.default_services is not None:
+		
+		print("服务产品 {} 被收藏 ".format(instance.default_services.service_sn))
+		fav_num = DefaultServices.objects.get(service_sn=instance.default_services.service_sn).service_fav_nums
+		print("服务产品 {} 当前收藏数为: {} ".format(instance.default_services.service_sn, fav_num))
+		if fav_num != 0:
+			fav_num -= 1
+		else:
+			fav_num = 0
+		
+		DefaultServices.objects.filter(service_sn=instance.default_services.service_sn).update(service_fav_nums=fav_num)
+		print("服务产品 {} 收藏数更新后为: {} ".format(instance.default_services.service_sn, fav_num))
+	else:
+		print("服务产品 {} 被收藏 ".format(instance.financing_services.service_sn))
+		fav_num = FinancingServices.objects.get(service_sn=instance.financing_services.service_sn).service_fav_nums
+		print("服务产品 {} 当前收藏数为: {} ".format(instance.financing_services.service_sn, fav_num))
+		if fav_num != 0:
+			fav_num -= 1
+		else:
+			fav_num = 0
+		
+		FinancingServices.objects.filter(service_sn=instance.financing_services.service_sn).update(
+			service_fav_nums=fav_num)
+		print("服务产品 {} 收藏数更新后为: {} ".format(instance.financing_services.service_sn, fav_num))
