40d2f068c2971019949a389e5dc2212e7200d0a1
warlock921 <caoyu921@163.com>
2019-09-30 17:25:17 +0800

    修复编辑器

--- HQJY_API/settings.py
+++ HQJY_API/settings.py
@@ -162,11 +162,11 @@ AUTHENTICATION_BACKENDS = (
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+# 	os.path.join(BASE_DIR, "static"),
+# )
 
 MEDIA_URL = "/media/"
 
