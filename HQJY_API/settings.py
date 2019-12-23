f5273958b43964c069aa087ca66ecf7d6fbf9b1f
warlock921 <caoyu921@163.com>
2019-12-23 17:24:26 +0800

    新增静态文件搜集

--- HQJY_API/settings.py
+++ HQJY_API/settings.py
@@ -180,10 +180,10 @@ AUTHENTICATION_BACKENDS = (
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = '/media/'
